3885eea61ba397cafcf0d6191a2f47e3557b76bd
domenicnauert <nauert.domenic@gmail.com>
2021-12-31 14:18:42 +0100

    hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  backendless: {
+    APP_ID: '8643CCDF-E094-60FE-FF6C-7B28D45AD200',
+    API_KEY: 'DA4B4851-EB9E-42FD-AB62-216AE67B3C88',
+  },
 };
