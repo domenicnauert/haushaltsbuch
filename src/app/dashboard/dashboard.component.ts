539ed35a52e3e27550288f92edac7895f391cda5
domenicnauert <nauert.domenic@gmail.com>
2022-01-09 20:45:04 +0100

    c

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -16,14 +16,13 @@ export class DashboardComponent {
 
   constructor() {
     const tab = localStorage.getItem(this.key);
-    console.log('loclastor ' + tab);
+
     if (tab) {
       this.selected = tab as unknown as number;
     }
   }
 
   selectedTabChanged(event: number) {
-    console.log('event ' + event);
     localStorage.setItem(this.key, event.toString());
     this.selected = event;
   }
